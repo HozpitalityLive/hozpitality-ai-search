976c2ec1c6abfa9f5dfcf311074216fbf9a5055b
kundanSoftech <kundan.softech@gmail.com>
2026-03-15 12:29:31 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ source env/bin/activate
 
 echo "Starting AI server..."
 
-nohup uvicorn ai_server:app --host 0.0.0.0 --port 8000 > ai_server.log 2>&1 &
+nohup uvicorn ai_server:app --host 0.0.0.0 --port 80 > ai_server.log 2>&1 &
