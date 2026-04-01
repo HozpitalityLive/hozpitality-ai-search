37b3b738e4e7373c321a9a8970702265c407a5c5
kundanSoftech <kundan.softech@gmail.com>
2026-04-01 19:13:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,10 @@ main_app = FastAPI()
 
 main_app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://hozpitality.com",
+        "https://www.hozpitality.com"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
