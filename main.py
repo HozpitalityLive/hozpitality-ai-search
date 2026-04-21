efb76b389fd4bf0be926a744b6868e77bfbbb69f
kundanSoftech <kundan.softech@gmail.com>
2026-04-21 14:43:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from ai_v2 import app as app2v2
 
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import WebSocket
+from ai_v2 import load_data, es
+import time
+import traceback
 
 
 main_app = FastAPI()
@@ -28,3 +31,34 @@ async def websocket_chat_main(websocket: WebSocket):
 main_app.mount("/v2", app1)
 main_app.mount("/app2v2", app2v2)
 # main_app.mount("", app1)
+
+
+@main_app.on_event("startup")
+def startup():
+    print("\n🚀 MAIN APP STARTUP", flush=True)
+
+    print("⏳ Waiting for Elasticsearch...", flush=True)
+
+    for i in range(30):
+        try:
+            print(f"🔁 ES {i+1}/30", flush=True)
+
+            if es.ping():
+                print("✅ Elasticsearch ready", flush=True)
+                time.sleep(5)
+                break
+        except Exception as e:
+            print("❌ ES error:", e, flush=True)
+
+        time.sleep(2)
+    else:
+        print("❌ ES NOT READY", flush=True)
+
+    print("🚀 Running load_data()", flush=True)
+
+    try:
+        load_data()
+        print("✅ load_data DONE", flush=True)
+    except Exception as e:
+        print("❌ load_data FAILED:", e, flush=True)
+        traceback.print_exc()
